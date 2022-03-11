5bd13f454913ad46a5502698d1f997a2bde779fc
Oguz <yilmazoguz@outlook.com>
2022-03-11 23:30:08 +0300

    Attach reducers with methods

--- src/features/restaurants/restaurantsSlice.js
+++ src/features/restaurants/restaurantsSlice.js
@@ -3,8 +3,20 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const restaurantsSlice = createSlice({
   name: "restaurants",
-  initialState: {},
-  reducers: {},
+  initialState: {
+    entities: [],
+  },
+  reducers: {
+    restaurantAdded(state,action){
+      state.entities.push({id: uuid(), name: action.payload})
+    },
+    restaurantDeleted(state,action){
+      const index = state.entities.findIndex(restaurant=>restaurant.id === action.payload);
+      state.entities.splice(index,1);
+    }
+  },
 });
+
+export const {restaurantAdded,restaurantDeleted} =  restaurantsSlice.actions;
 
 export default restaurantsSlice.reducer;
